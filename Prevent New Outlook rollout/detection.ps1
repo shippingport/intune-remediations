2baaff1c2d0cdccb2133b59dd9a58ba705e327dc
Christian Wedema <61030366+shippingport@users.noreply.github.com>
2025-01-13 15:15:53 +0100

    Added cleanup
    Added PSDrive cleanup

--- Prevent New Outlook rollout/detection.ps1
+++ Prevent New Outlook rollout/detection.ps1
@@ -1,5 +1,4 @@
 # Detection script for NewOutlookMigrationUserSetting.
-# Run the script as the system user.
 
 # Get the current user's SID
 $SID = (New-Object -ComObject Microsoft.DiskQuota).TranslateLogonNameToSID((Get-CimInstance -ClassName Win32_ComputerSystem).Username)
@@ -22,16 +21,28 @@ If([string]::IsNullOrEmpty($SID)) {
         If($Result -ne 0) {
             Write-Warning "Not compliant: NewOutlookMigrationUserSetting is not correctly, value is currently: $Result. Run remediation."
             Write-Host "Exit 0"
-            #Exit 0
+            
+            # Clean up
+            Remove-PSDrive -Name HKU
+
+            Exit 0
         } else {
             Write-Host "Compliant: NewOutlookMigrationUserSetting is set correctly."
             Write-Host "Exit 1"
+            
+            # Clean up
+            Remove-PSDrive -Name HKU
+            
             Exit 1
         }
     } else {
         #false
         Write-Warning "Not compliant: NewOutlookMigrationUserSetting does not exist. Run remediation."
         Write-Host "Exit 0"
+        
+        # Clean up
+        Remove-PSDrive -Name HKU
+
         Exit 1
     }
 }
