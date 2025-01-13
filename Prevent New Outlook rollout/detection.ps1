b2658370f9cce9bd5b77ed62ea4ff4e12be40124
Christian Wedema <61030366+shippingport@users.noreply.github.com>
2025-01-13 15:13:55 +0100

    Updated detection mechanism
    Changed detection mechanism

--- Prevent New Outlook rollout/detection.ps1
+++ Prevent New Outlook rollout/detection.ps1
@@ -1,19 +1,37 @@
-$Path = "HKCU:\Software\Policies\Microsoft\Office\16.0\Outlook\Preferences"
-$Name = "NewOutlookMigrationUserSetting"
+# Detection script for NewOutlookMigrationUserSetting.
+# Run the script as the system user.
 
-If(Test-Path -Path "$Path\$Name") {
-    # true
-    $Result = Get-ItemProperty -Path $Path -Name $Name
+# Get the current user's SID
+$SID = (New-Object -ComObject Microsoft.DiskQuota).TranslateLogonNameToSID((Get-CimInstance -ClassName Win32_ComputerSystem).Username)
 
-    If($Result.NewOutlookMigrationUserSetting -ne 0) {
-        Write-Warning "Not compliant: NewOutlookMigrationUserSetting is not correctly, value is currently: $Result. Run remediation."
-        Exit 0
+If([string]::IsNullOrEmpty($SID)) {
+    Write-Warning "NewOutlookMigrationUserSetting: no user currently logged in. Exiting."
+    Exit 0
+} else {
+    # Add the HKU drive since this is not mapped by default
+    New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS | Out-Null
+
+    $Path = "registry::HKU\$SID\Software\Policies\Microsoft\Office\16.0\Outlook\Preferences"
+    $Name = "NewOutlookMigrationUserSetting"
+
+    If((Get-Item -Path $Path).Property -Contains $Name) {
+        # Path exits, check its value
+        $Result = Get-ItemProperty -Path $Path -Name $Name
+        $Result = $Result.NewOutlookMigrationUserSetting
+    
+        If($Result -ne 0) {
+            Write-Warning "Not compliant: NewOutlookMigrationUserSetting is not correctly, value is currently: $Result. Run remediation."
+            Write-Host "Exit 0"
+            #Exit 0
+        } else {
+            Write-Host "Compliant: NewOutlookMigrationUserSetting is set correctly."
+            Write-Host "Exit 1"
+            Exit 1
+        }
     } else {
-        Write-Host "Compliant: NewOutlookMigrationUserSetting is set correctly."
+        #false
+        Write-Warning "Not compliant: NewOutlookMigrationUserSetting does not exist. Run remediation."
+        Write-Host "Exit 0"
         Exit 1
     }
-} else {
-    #false
-    Write-Warning "Not compliant: NewOutlookMigrationUserSetting does not exist. Run remediation."
-    Exit 1
 }
