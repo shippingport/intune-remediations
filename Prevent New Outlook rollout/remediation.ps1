caed44b720894d942fe81304f0359f232745ad62
Christian Wedema <61030366+shippingport@users.noreply.github.com>
2025-01-14 10:23:13 +0100

    Update remediation.ps1

--- Prevent New Outlook rollout/remediation.ps1
+++ Prevent New Outlook rollout/remediation.ps1
@@ -1,9 +1,22 @@
-$Path = "HKCU:\Software\Policies\Microsoft\Office\16.0\Outlook\Preferences"
-$Name = "NewOutlookMigrationUserSetting"
+# Remediation script for NewOutlookMigrationUserSetting.
 
-If(!(Test-Path $Path)) {
-    New-Item -Path $Path -Force
+# Get the current user's SID
+$SID = (New-Object -ComObject Microsoft.DiskQuota).TranslateLogonNameToSID((Get-CimInstance -ClassName Win32_ComputerSystem).Username)
+
+If([string]::IsNullOrEmpty($SID)) {
+    Write-Warning "NewOutlookMigrationUserSetting: no user currently logged in. Exiting."
+    Exit 0
+} else {
+    # Add the HKU drive since this is not mapped by default
+    New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS | Out-Null
+
+    $Path = "registry::HKU\$SID\Software\Policies\Microsoft\Office\16.0\Outlook\Preferences"
+    $Name = "NewOutlookMigrationUserSetting"
+
+    New-ItemProperty -Path $Path -Name $Name -Value 0 -PropertyType DWORD -Force
+    Write-Host "Remediation: added NewOutlookMigrationUserSetting with value 0."
+    
+    Write-Host "Remediation: performing clean up."
+    # Clean up
+    Remove-PSDrive -Name HKU
 }
-
-New-ItemProperty -Path $Path -Name $Name -Value 0 -PropertyType DWORD -Force
-Write-Host "Remediation: added NewOutlookMigrationUserSetting with value 0."
